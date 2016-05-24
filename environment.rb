3ee00fb1f06357e16e4c253a544c1f4180da16b7
Hoa Nguyen <hoathenguyen85@gmail.com>
2016-05-24 12:33:14 -0500

    update environment to database and model

--- environment.rb
+++ environment.rb
@@ -8,9 +8,13 @@ require 'rubygems'
 require 'uri'
 require 'pathname'
 
+require APP_ROOT.join('database')
 
 APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
 #Load Controllers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
+
+#Load Model
+Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
